0efa0463816babddd55da49471d71f6b9f65b2fc
Joel Truher <joel@truher.org>
2021-06-28 10:43:46 -0700

    simplification, cleanup
    free running loop seems to sample every 5 ms,
    2/1 for accel, maybe more would be better.

--- Artemis/Artemis.ino
+++ Artemis/Artemis.ino
@@ -1,6 +1,6 @@
 // rocket instrument
-#include <BMP388_DEV.h>
-#include <Wire.h>
+#include "BMP388_DEV.h"
+#include "Wire.h"
 #include "SdFat.h"
 #include "SparkFun_Qwiic_KX13X.h"
 
@@ -20,14 +20,30 @@ uint32_t us = 0;
 
 void setup() {
   pinMode(blinkPin_OLA, OUTPUT);
+  digitalWrite(blinkPin_OLA, HIGH);
+  delay(1000);
+  digitalWrite(blinkPin_OLA, LOW);
+  
+  Serial.begin(115200);
+  Serial.println("setup");
 
   // ============== SD ==============
 
-  if (!SD.begin(23)) return;
+  if (!SD.begin(23)) {
+    Serial.println("sd file fail");
+    return;
+  }
   kxFile = SD.open("kx.txt", FILE_WRITE);
-  if (!kxFile) return;
-  bmpFile = SD.open("bmp.txt", FILE_WRITE);  
-  if (!bmpFile) return;
+  if (!kxFile) {
+    Serial.println("kx file fail");
+    return;
+  }
+  bmpFile = SD.open("bmp.txt", FILE_WRITE);
+  if (!bmpFile) {
+    Serial.println("bmp file fail");
+    return;
+  }
+  Serial.println("sd done");
 
   // ============== I2C ==============
 
@@ -35,20 +51,57 @@ void setup() {
   pinMode(PIN_QWIIC_POWER, OUTPUT);
   digitalWrite(PIN_QWIIC_POWER, HIGH);
   Wire.begin();
-  delay(100); // essential to wait for the i2c boards to power up
+  Wire.setClock(1000000);  // fast mode plus seems to be the KX max speed
+  Wire.setPullups(1); // this is what the OLA code uses
+  delay(1000); // essential to wait for the i2c boards to power up
+  Serial.println("i2c done");
 
   // ============== ACCELEROMETER ==============
 
-  if (!kxAccel.begin(KX13X_DEFAULT_ADDRESS, Wire) ) return;
-  if ( !kxAccel.initialize(DEFAULT_SETTINGS)) return;
-  kxAccel.setRange(KX134_RANGE64G);
-  
+  if (!kxAccel.begin(KX13X_DEFAULT_ADDRESS, Wire)) {
+    Serial.println("kx begin fail");
+    return;
+  }
+  if (!kxAccel.initialize(DEFAULT_SETTINGS)) {
+    Serial.println("kx init fail");
+    return;
+  }
+  if (!kxAccel.setRange(KX134_RANGE64G)) {
+    Serial.println("kx range fail");
+    return;
+  }
+
+  KX13X_STATUS_t returnError;
+  returnError = kxAccel.writeRegister(KX13X_CNTL1, 0x7f, 0, 7); // standby mode for this change
+  if ( returnError != KX13X_SUCCESS ) {
+    Serial.println("standby mode fail");
+    return;
+  }
+  if (!kxAccel.setOutputDataRate(0x08)) { // 200hz
+    Serial.println("ODR fail");
+    return;
+  }
+  returnError = kxAccel.writeRegister(KX13X_ODCNTL, 0x7f, 1, 7); // bypass IIR filter
+  if ( returnError != KX13X_SUCCESS ) {
+    Serial.println("IIR fail");
+    return;
+  }
+  returnError = kxAccel.writeRegister(KX13X_CNTL1, 0x7f, 1, 7); // back to normal mode
+  if ( returnError != KX13X_SUCCESS ) {
+    Serial.println("normal mode fail");
+    return;
+  }
+
+  Serial.println("kx done");
+
   // ============== BAROMETER ==============
 
   if (!bmp388.begin(BMP388_I2C_ALT_ADDR)) return;
-  bmp388.setTimeStandby(TIME_STANDBY_320MS);
-  bmp388.startNormalConversion();
+  bmp388.setTempOversampling(OVERSAMPLING_SKIP);
+  bmp388.setPresOversampling(OVERSAMPLING_SKIP);
+  bmp388.startForcedConversion();
   initialized = true;
+  Serial.println("done");
 }
 
 void loop() {
@@ -61,32 +114,34 @@ void loop() {
   if (counter > 50) {
     digitalWrite(blinkPin_OLA, LOW);
   }
+
   // ============== ACCELEROMETER ==============
-  // goes at about 200hz, will integrate to find speed.
-  
+
   myData = kxAccel.getAccelData();
   us = micros();
   kxFile.print(us);
   kxFile.print("\t");
-  kxFile.print(myData.xData);
+  kxFile.print(myData.xData, 4);
   kxFile.print("\t");
-  kxFile.print(myData.yData);
+  kxFile.print(myData.yData, 4);
   kxFile.print("\t");
-  kxFile.println(myData.zData);
-  kxFile.sync();
+  kxFile.println(myData.zData, 4);
+  kxFile.flush();
+
+  if (counter % 2) return; // take twice as many accel samples.
 
   // ============== BAROMETER ==============
-  // can go much more slowly, it's just looking for apogee
-  
+
   if (bmp388.getMeasurements(temperature, pressure, altitude)) {
     us = micros();
     bmpFile.print(us);
     bmpFile.print("\t");
-    bmpFile.print(temperature); // degrees C
+    bmpFile.print(temperature, 4); // degrees C
     bmpFile.print("\t");
-    bmpFile.print(pressure); // hectopascals, i.e. millibar
+    bmpFile.print(pressure, 4); // hectopascals, i.e. millibar
     bmpFile.print("\t");
-    bmpFile.println(altitude); // meters
-    bmpFile.sync();
+    bmpFile.println(altitude, 4); // meters
+    bmpFile.flush();
+    bmp388.startForcedConversion();
   }
 }
